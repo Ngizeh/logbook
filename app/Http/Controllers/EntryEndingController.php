bed3962f2a709bee58ee8986033294b84d125b3c
Ngizeh Mwas <edngize@gmail.com>
2021-04-12 14:53:44 +0300

    Entire week ending backend entries can be queried

--- app/Http/Controllers/EntryEndingController.php
+++ app/Http/Controllers/EntryEndingController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Entry;
 use Illuminate\Http\Request;
 
 class EntryEndingController extends Controller
 {
-    //
+    public function index(string $date)
+    {
+        $entries = Entry::forWeekEnding($date)->get();
+
+        return view('entries.table', compact('entries'));
+    }
 }
--- tests/Feature/EntryWeekEndingTest.php
+++ tests/Feature/EntryWeekEndingTest.php
@@ -2,21 +2,37 @@
 
 namespace Tests\Feature;
 
+use App\Entry;
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class EntryWeekEndingTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test **/
+    public function can_get_entries_for_week_ending_in_a_given_week()
+    {
+        $this->withoutExceptionHandling();
+
+        Carbon::setTestNow('Friday April 9th, 2021');
+        $thisWeekEntry = factory(Entry::class)->create();
+        $lastWeekEntry = factory(Entry::class)->create(['created_at' => now()->subWeek()]);
+
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user)->get(route('entries.weekending', 'April 9th, 2021'))
+            ->assertViewHas('entries', function($entries) use ($thisWeekEntry, $lastWeekEntry){
+                if(!$entries->contains($thisWeekEntry)){
+                    $this->fail('This week entries are not shown');
+                }
+                if($entries->contains($lastWeekEntry)){
+                    $this->fail('Found last week entries');
+                }
+                return true;
+            });
     }
 }
