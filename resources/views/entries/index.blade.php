cbca9d4d24d0dd51f47d029f5405b16ac64dd241
Ngizeh Mwas <edngize@gmail.com>
2021-04-12 13:36:18 +0300

    UI for Weeks entres are displayed

--- resources/views/entries/index.blade.php
+++ resources/views/entries/index.blade.php
@@ -7,9 +7,13 @@
 			<div class="card">
 				<div class="card-header">
 					<div class="row">
-						<div class="col-md-6">
-							Entries
-						</div>
+                        <div class="col-md-6">
+                            <select name="" id="" class="form-control">
+                                @foreach($entriesDate as $date)
+                                    <option value="{{ $date }}">Week Ending {{ $date }}</option>
+                                @endforeach
+                            </select>
+                        </div>
 						<div class="col-md-6 text-right">
 							<a href="{{ route('entries.create') }}" class="btn btn-success">Add</a>
 						</div>
