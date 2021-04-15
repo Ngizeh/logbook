8260aec3933c2afdf90b61016b49ec6c7df6852a
Ngizeh Mwas <edngize@gmail.com>
2021-04-15 05:18:46 +0300

    Refactored Edit to proper fields lists

--- resources/js/components/Entries/Create.vue
+++ resources/js/components/Entries/Create.vue
@@ -7,25 +7,62 @@
                     <form class="form-group px-4" @submit.prevent="addLog">
                         <div class="form-group">
                             <label for="title">Title:</label>
-                            <input type="text" class="form-control" name="title" v-model="form.title" id="title">
-                            <span v-if="errors" class="text-danger">{{ errors.title[0] }}</span>
+                            <input
+                                type="text"
+                                class="form-control"
+                                name="title"
+                                v-model="form.title"
+                                id="title"
+                            />
+                            <span v-if="errors" class="text-danger">{{
+                                errors.title[0]
+                            }}</span>
                         </div>
                         <div class="form-group">
                             <label for="category_id">Type:</label>
-                            <select name="category_id" id="category_id" class="form-control" v-model="form.category_id">
-                                <option selected disabled value="">Choose your category</option>
-                                <option v-for="category in categories" :value="category.id">{{ category.name }}</option>
+                            <select
+                                name="category_id"
+                                id="category_id"
+                                class="form-control"
+                                v-model="form.category_id"
+                            >
+                                <option selected disabled value="">
+                                    Choose your category
+                                </option>
+                                <option
+                                    v-for="(category, index) in categories"
+                                    :key="index"
+                                    :value="category.id"
+                                >
+                                    {{ category.name }}
+                                </option>
                             </select>
-                            <span v-if="errors" class="text-danger">{{ errors.category_id[0] }}</span>
+                            <span v-if="errors" class="text-danger">{{
+                                errors.category_id[0]
+                            }}</span>
                         </div>
                         <div class="form-group">
                             <label for="description">Description:</label>
-                            <textarea type="text" class="form-control" name="description" id="description" v-model="form.description"></textarea>
-                            <span v-if="errors" class="text-danger">{{ errors.description[0] }}</span>
+                            <textarea
+                                type="text"
+                                class="form-control"
+                                name="description"
+                                id="description"
+                                v-model="form.description"
+                            ></textarea>
+                            <span v-if="errors" class="text-danger">{{
+                                errors.description[0]
+                            }}</span>
                         </div>
                         <div class="d-flex justify-content-between">
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                            <a :href="route('entries.index')" class="btn btn-link">Cancel</a>
+                            <button type="submit" class="btn btn-primary">
+                                Submit
+                            </button>
+                            <a
+                                :href="route('entries.index')"
+                                class="btn btn-link"
+                                >Cancel</a
+                            >
                         </div>
                     </form>
                 </div>
--- resources/js/components/Entries/Edit.vue
+++ resources/js/components/Entries/Edit.vue
@@ -13,15 +13,15 @@
                         </div>
                         <div class="form-group">
                             <label for="category_id">Category:</label>
-                            <select name="category_id" id="category_id" class="form-control">
-                                <option selected disabled>Choose your category</option>
-                                <option v-for="category in categories" v-model="form.category" >{{ category.name }}</option>
+                            <select name="category_id" id="category_id" class="form-control" v-model="form.category_id">
+                                <option selected disabled value="">Choose your category</option>
+                                <option v-for="(category, index) in categories" :key="index" :value="category.id" >{{ category.name }}</option>
                             </select>
                             <span v-if="errors" class="text-danger">{{ errors.category_id[0] }}</span>
                         </div>
                         <div class="form-group">
                             <label for="description">Description:</label>
-                            <textarea type="text" class="form-control" name="description" id="description">{{ form.description }}</textarea>
+                            <textarea type="text" class="form-control" name="description" id="description" v-model="form.description"></textarea>
                             <span v-if="errors" class="text-danger">{{ errors.description[0] }}</span>
                         </div>
                         <div class="d-flex justify-content-between">
@@ -44,16 +44,17 @@ export default {
             form: {
                 title: this.entry.title,
                 description: this.entry.description,
-                category: this.entry.category_id,
+                category_id: this.entry.category_id,
             },
             errors : ""
         }
-    },
+    }, 
     methods: {
         editLog(){
-            axios.patch(route('entries.update'), this.form)
+            axios.patch(route('entries.update', this.entry), this.form)
                  .then(() => {
-                     axios.get(route('entries.index'))
+                     this.form = {};
+                     window.location.href = route('entries.index')
                  }).catch(error => {
                      this.errors = error.response.data.errors
             })
